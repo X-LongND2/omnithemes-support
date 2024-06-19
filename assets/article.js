dd5791f9aad03e2a237aa2dd675ddfe219573000
X-LongND2 <longnd2@bsscommerce.com>
2024-06-19 17:31:16 +0700

    Fix bug outline

--- assets/article.js
+++ assets/article.js
@@ -9,25 +9,41 @@ if (!window.Eurus.loadedScript.includes('article.js')) {
             entries.forEach((entry) => {
               // If heading is in the top 30% of the screen
               if (entry.isIntersecting) {
-                  if(document.querySelector('.menu-article .active')) {
-                    document.querySelector('.menu-article .active').classList.remove("active");
-                  }
+                if (document.querySelector('.menu-article .active')) {
+                  document.querySelector('.menu-article .active').classList.remove("active");
+                }
                 document.querySelectorAll('.item-menu-article')[entry.target.dataset.index].classList.add("active");
               }
             });
-          }, {rootMargin: '0px 0px -60% 0px'});
-          if(document.querySelectorAll('article h2, article h3').length > 1 ) {
+          }, { rootMargin: '0px 0px -60% 0px' });
+          if (document.querySelectorAll('article h2, article h3').length > 1) {
             document.querySelectorAll('article h2, article h3').forEach((heading, index) => {
               heading.dataset.index = index;
               observer.observe(heading);
             });
-          }else {
+          } else {
             document.querySelector('.menu-article').remove()
           }
         },
         loadData(el) {
-          if (window.innerWidth >= 1024) {
-            el.style.marginTop = document.querySelector(".page__body").offsetTop + 30 +"px";
+          const heading2 = document.querySelectorAll('article h2, article h3');
+          if (heading2.length > 1) {
+            heading2.forEach((item, index) => {
+              let htmlContent = "<li class='item-menu-article w-full cursor-pointer pb-2 " + (item.tagName == 'H3' ? 'pl-3' : '') + "' @click='scrollTop($el," + index + ")' >" + item.textContent + "</li>";
+              document.querySelector('.list-menu-article').innerHTML += htmlContent;
+              if (index === heading2.length - 1) {
+                setTimeout(() => {
+                  const outlineHeight = window.getComputedStyle(el).height   
+                  if(document.body.clientWidth < 1024) {
+                    const pageBody = document.querySelector('.page__body')
+                    pageBody.style.marginTop = `calc(${Number(outlineHeight.slice(0, outlineHeight.length - 2))}px)`
+                  }
+                }, 100)
+              }
+            })
+          }
+          if (document.body.clientWidth >= 1024) {
+            el.style.marginTop = document.querySelector(".page__body").offsetTop + 30 + "px";
           }
           else {
             el.style.display = 'block'
@@ -37,29 +53,20 @@ if (!window.Eurus.loadedScript.includes('article.js')) {
               return rect.top + scrollTop
             }
             const pageTitle = document.querySelector('.page__title')
-            const pageBody =  document.querySelector('.page__body')
+            
             const pageContainer = document.querySelector('.page__container')
             const pageTitleHeight = window.getComputedStyle(pageTitle).height
-            const outlineHeight = window.getComputedStyle(el).height
-            const outlinePos = getElementDocumentOffset(pageTitle) - getElementDocumentOffset(pageContainer) + Number(pageTitleHeight.slice(0, pageTitleHeight.length-2))
+            const outlinePos = getElementDocumentOffset(pageTitle) - getElementDocumentOffset(pageContainer) + Number(pageTitleHeight.slice(0, pageTitleHeight.length - 2))
             el.style.top = `calc(${outlinePos}px + 1rem)`
-            if(window.innerWidth >= 768) {
+            if (window.innerWidth >= 768) {
               el.style.marginLeft = '20px'
             }
-            el.style.padding = '0px 20px'
-            pageBody.style.marginTop = `calc(${Number(outlineHeight.slice(0, outlineHeight.length-2))}px + 2rem)`
-          }
-          const heading2 = document.querySelectorAll('article h2, article h3');
-          if(heading2.length > 1 ) {
-            heading2.forEach((item, index) => {
-              let htmlContent = "<li class='item-menu-article w-full cursor-pointer pb-2 "+ (item.tagName == 'H3' ? 'pl-3' : '') +"' @click='scrollTop($el," + index + ")' >" + item.textContent + "</li>";
-              document.querySelector('.list-menu-article').innerHTML += htmlContent;
-            })
+            el.style.padding = '0px 20px'      
           }
         },
-        scrollTop(el,index) {
-          if(document.querySelectorAll('article h2, article h3').length > index) {
-            if(document.querySelector('.menu-article .active')) {
+        scrollTop(el, index) {
+          if (document.querySelectorAll('article h2, article h3').length > index) {
+            if (document.querySelector('.menu-article .active')) {
               document.querySelector('.menu-article .active').classList.remove("active");
             }
             el.classList.add("active");
